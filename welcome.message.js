16d67eb32f6596978ba55957036baf4422e5d1c4
Florent LE BOULCH <florent.le.boulch@gmail.com>
2019-04-28 18:29:41 +0200

    Update README

--- welcome.message.js
+++ welcome.message.js
@@ -3,9 +3,9 @@ const chalk = require('chalk');
 const msg = `
 Welcome to the ${chalk.blue.bold('Ionic')} ${chalk.green.bold('J')}${chalk.red.bold('Hipster')} Starter!
 
-The Ionic JHipster Starter ships with login and signup that will work with a JHipster backend (with JWT Authentication). 
+The Ionic JHipster Starter ships with login and signup that will work with a JHipster backend (with JWT Authentication).
 
-For more details, please see the project's README: ${chalk.bold('https://github.com/oktadeveloper/ionic-jhipster-starter/blob/master/README.md')}
+For more details, please see the project's README: ${chalk.bold('https://github.com/fleboulch/ionic-jhipster-starter/blob/master/README.md')}
 `.trim()
 
 console.log(msg);
