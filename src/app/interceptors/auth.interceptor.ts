16d56d26662730c3be4d748fc6094bd9cd99c922
Matt Raible <matt.raible@okta.com>
2019-06-11 14:09:35 -0600

    Fix interceptor; remove console errors

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,26 +1,32 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { ApiService } from '../services/api/api.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private localStorage: LocalStorageService, private sessionStorage: SessionStorageService) {}
+  constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!request || !request.url || (/^http/.test(request.url) && !(ApiService.API_URL && request.url.startsWith(ApiService.API_URL)))) {
       return next.handle(request);
     }
 
-    const token = this.localStorage.retrieve('authenticationToken') || this.sessionStorage.retrieve('authenticationToken');
-    if (!!token) {
+    return from(this.handleAccess(request, next));
+  }
+
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    try {
+      const token = await this.authService.getValidToken();
       request = request.clone({
         setHeaders: {
-          Authorization: 'Bearer ' + token
+          Authorization: `${token.tokenType} ${token.accessToken}`
         }
       });
+    } catch {
+      // ignore
     }
-    return next.handle(request);
+    return next.handle(request).toPromise();
   }
 }
