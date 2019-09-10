366a6a97c3767e77740e39d057b16df0184cdc8d
Matt Raible <matt.raible@okta.com>
2019-09-10 11:58:28 +0100

    Update dependencies and welcome message (#33)

--- welcome.message.js
+++ welcome.message.js
@@ -3,8 +3,6 @@ const chalk = require('chalk');
 const msg = `
 Welcome to the ${chalk.blue.bold('Ionic')} ${chalk.green.bold('J')}${chalk.red.bold('Hipster')} Starter!
 
-The Ionic JHipster Starter ships with login and signup that will work with a JHipster backend.
-
 For more details, please see the project's README: ${chalk.bold('https://github.com/oktadeveloper/ionic-jhipster-starter/blob/master/README.md')}
 `.trim()
 
