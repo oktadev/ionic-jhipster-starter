34d0aac6f643781fbb16b9d4f77dc4f1298ae959
Matt Raible <matt.raible@okta.com>
2020-04-04 11:49:55 -0600

    Remove tsconfig.e2e.json in favor of existing one (#43)

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -19,7 +19,7 @@ exports.config = {
   },
   onPrepare() {
     require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
+      project: 'e2e/tsconfig.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: 'pretty' } }));
   }
